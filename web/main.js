96466a9b7ad49c32f1b1757e88354104a5b41806
Jochem <jochemdejaeghere@hotmail.com>
2019-04-01 13:41:11 +0200

    Added website content

---
+++ web/main.js
@@ -0,0 +1,50 @@
+var NotificationTime = /** @class */ (function () {
+    function NotificationTime() {
+    }
+    return NotificationTime;
+}());
+var Notificationtimes = /** @class */ (function () {
+    function Notificationtimes() {
+    }
+    return Notificationtimes;
+}());
+var Subscription = /** @class */ (function () {
+    function Subscription() {
+    }
+    return Subscription;
+}());
+var Verification = /** @class */ (function () {
+    function Verification() {
+    }
+    return Verification;
+}());
+function getNotificationTime(day) {
+    var notificationTime = new NotificationTime();
+    notificationTime.Hour = +document.getElementById(day + 'Hour').value;
+    notificationTime.Minute = +document.getElementById(day + 'Minute').value;
+    return notificationTime;
+}
+function subscribe() {
+    var notificationTimes = new Notificationtimes();
+    notificationTimes.Monday = getNotificationTime('monday');
+    notificationTimes.Tuesday = getNotificationTime('tuesday');
+    notificationTimes.Wednesday = getNotificationTime('wednesday');
+    notificationTimes.Thursday = getNotificationTime('thursday');
+    notificationTimes.Friday = getNotificationTime('friday');
+    var subscription = new Subscription();
+    subscription.Email = document.getElementById('email').value;
+    subscription.NotificationTimes = notificationTimes;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', 'https://pxlcateringnotification.azurewebsites.net/api/subscribe', true);
+    xmlhttp.setRequestHeader('Content-type', 'application/json');
+    xmlhttp.send(JSON.stringify(subscription));
+}
+function verify() {
+    var verification = new Verification();
+    verification.Email = document.getElementById('email').value;
+    verification.Token = document.getElementById('token').value;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', 'https://pxlcateringnotification.azurewebsites.net/api/verify', true);
+    xmlhttp.setRequestHeader('Content-type', 'application/json');
+    xmlhttp.send(JSON.stringify(verification));
+}
---
+++ web/main.ts
@@ -0,0 +1,60 @@
+class NotificationTime {
+    Hour: number;
+    Minute: number;
+}
+
+class Notificationtimes {
+    Monday: NotificationTime;
+    Tuesday: NotificationTime;
+    Wednesday: NotificationTime;
+    Thursday: NotificationTime;
+    Friday: NotificationTime;
+}
+
+class Subscription {
+    Email: string;
+    NotificationTimes: Notificationtimes;
+}
+
+class Verification {
+    Email: string;
+    Token: string;
+}
+
+function getNotificationTime(day: string): NotificationTime {
+    let notificationTime = new NotificationTime();
+    notificationTime.Hour = +(<HTMLInputElement>document.getElementById(day + 'Hour')).value;
+    notificationTime.Minute = +(<HTMLInputElement>document.getElementById(day + 'Minute')).value;
+
+    return notificationTime;
+}
+
+function subscribe() {
+    let notificationTimes = new Notificationtimes();
+    notificationTimes.Monday = getNotificationTime('monday');
+    notificationTimes.Tuesday = getNotificationTime('tuesday');
+    notificationTimes.Wednesday = getNotificationTime('wednesday');
+    notificationTimes.Thursday = getNotificationTime('thursday');
+    notificationTimes.Friday = getNotificationTime('friday');
+
+    let subscription = new Subscription();
+    subscription.Email = (<HTMLInputElement>document.getElementById('email')).value;
+    subscription.NotificationTimes = notificationTimes;
+
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', 'https://pxlcateringnotification.azurewebsites.net/api/subscribe', true);
+    xmlhttp.setRequestHeader('Content-type', 'application/json');
+    xmlhttp.send(JSON.stringify(subscription));
+}
+
+function verify() {
+    let verification = new Verification();
+    verification.Email = (<HTMLInputElement>document.getElementById('email')).value;
+    verification.Token = (<HTMLInputElement>document.getElementById('token')).value;
+
+
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', 'https://pxlcateringnotification.azurewebsites.net/api/verify', true);
+    xmlhttp.setRequestHeader('Content-type', 'application/json');
+    xmlhttp.send(JSON.stringify(verification));
+}
