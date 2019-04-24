9383cd93aa5041a16512504531f64a113cbaaef1
Joachim Veulemans <dj-joa@hotmail.be>
2019-04-24 23:00:42 +0200

    Feature so you can chose where you eat each day & disabling a day

--- web/main.js
+++ web/main.js
@@ -18,6 +18,14 @@ var Verification = /** @class */ (function () {
     }
     return Verification;
 }());
+var Days;
+(function (Days) {
+    Days["monday"] = "monday";
+    Days["tuesday"] = "tuesday";
+    Days["wednesday"] = "wednesday";
+    Days["thursday"] = "thursday";
+    Days["friday"] = "friday";
+})(Days || (Days = {}));
 function getNotificationTime(day) {
     var notificationTime = new NotificationTime();
     var time = document.getElementById(day).value.split(':');
@@ -26,15 +34,10 @@ function getNotificationTime(day) {
     return notificationTime;
 }
 function subscribe() {
-    var notificationTimes = new Notificationtimes();
-    notificationTimes.Monday = getNotificationTime('monday');
-    notificationTimes.Tuesday = getNotificationTime('tuesday');
-    notificationTimes.Wednesday = getNotificationTime('wednesday');
-    notificationTimes.Thursday = getNotificationTime('thursday');
-    notificationTimes.Friday = getNotificationTime('friday');
     var subscription = new Subscription();
     subscription.Email = document.getElementById('email').value;
-    subscription.NotificationTimes = notificationTimes;
+    subscription.Locations = getLocations();
+    subscription.NotificationTimes = getNotificationsTime();
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open('POST', 'https://pxlcateringnotification.azurewebsites.net/api/subscribe', true);
     xmlhttp.setRequestHeader('Content-type', 'application/json');
@@ -44,9 +47,18 @@ function subscribe() {
             alert("Oeps, er trad een error op! Weet je zeker dat alle gegevens correct zijn?");
         }
         else {
-            window.location.href = "https://cateringstorage.z6.web.core.windows.net/verify?email=" + subscription.Email;
+            // window.location.href = "https://cateringstorage.z6.web.core.windows.net/verify?email=" + subscription.Email;
         }
     };
+}
+function getNotificationsTime() {
+    var notificationTimes = new Notificationtimes();
+    Object.keys(Days).forEach(function (day) {
+        if (document.getElementById('no_' + day).checked === false) {
+            notificationTimes[day] = getNotificationTime(day);
+        }
+    });
+    return notificationTimes;
 }
 function verify() {
     var verification = new Verification();
@@ -70,7 +82,7 @@ function getEmailFromURL() {
     var email = url.searchParams.get("email");
     document.getElementById('email').value = email;
 }
-function checkInput() {
+function checkTokenInput() {
     var expression = /\S+@\S+/;
     if (document.getElementById('token').value.length === 6 && expression.test(String(document.getElementById('email').value).toLowerCase())) {
         document.getElementById('submit').disabled = false;
@@ -79,7 +91,7 @@ function checkInput() {
         document.getElementById('submit').disabled = true;
     }
 }
-function checkInputEmail() {
+function checkEmailInput() {
     var expression = /\S+@\S+/;
     if (expression.test(String(document.getElementById('email').value).toLowerCase())) {
         document.getElementById('submit').disabled = false;
@@ -87,4 +99,36 @@ function checkInputEmail() {
     else {
         document.getElementById('submit').disabled = true;
     }
+}
+function no_email_clicked(day) {
+    var timeElement = document.getElementById(day);
+    var locationElement = document.getElementById(day + "_location");
+    console.log(timeElement);
+    console.log(locationElement);
+    if (document.getElementById('no_' + day).checked === true) {
+        timeElement.value = "--:--";
+        timeElement.disabled = true;
+        locationElement.value = "";
+        locationElement.disabled = true;
+    }
+    else {
+        timeElement.value = "11:45";
+        timeElement.disabled = false;
+        locationElement.selectedIndex = 0;
+        locationElement.disabled = false;
+    }
+}
+function getLocations() {
+    var locations = '{';
+    Object.keys(Days).forEach(function (day) {
+        if (document.getElementById(day + "_location").disabled === false) {
+            locations += '"' + day + '": "' + (document.getElementById(day + "_location").selectedOptions[0].value) + '",';
+        }
+    });
+    locations = locations.substring(0, locations.length - 1) + '}';
+    console.log(locations);
+    if (locations.length < 5) {
+        return JSON.parse('{}');
+    }
+    return JSON.parse(locations);
 }
--- web/main.ts
+++ web/main.ts
@@ -13,12 +13,21 @@ class Notificationtimes {
 
 class Subscription {
     Email: string;
+    Locations: string[];
     NotificationTimes: Notificationtimes;
 }
 
 class Verification {
     Email: string;
     Token: string;
+}
+
+enum Days {
+    monday = "monday",
+    tuesday = "tuesday",
+    wednesday = "wednesday",
+    thursday = "thursday",
+    friday = "friday"
 }
 
 function getNotificationTime(day: string): NotificationTime {
@@ -31,16 +40,10 @@ function getNotificationTime(day: string): NotificationTime {
 }
 
 function subscribe() {
-    let notificationTimes = new Notificationtimes();
-    notificationTimes.Monday = getNotificationTime('monday');
-    notificationTimes.Tuesday = getNotificationTime('tuesday');
-    notificationTimes.Wednesday = getNotificationTime('wednesday');
-    notificationTimes.Thursday = getNotificationTime('thursday');
-    notificationTimes.Friday = getNotificationTime('friday');
-
     let subscription = new Subscription();
     subscription.Email = (<HTMLInputElement>document.getElementById('email')).value;
-    subscription.NotificationTimes = notificationTimes;
+    subscription.Locations = getLocations();
+    subscription.NotificationTimes = getNotificationsTime();
 
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.open('POST', 'https://pxlcateringnotification.azurewebsites.net/api/subscribe', true);
@@ -49,17 +52,27 @@ function subscribe() {
     xmlhttp.onload = function() {
         if (xmlhttp.status != 200) {
           alert(`Oeps, er trad een error op! Weet je zeker dat alle gegevens correct zijn?`);
-        } else {
-            window.location.href = "https://cateringstorage.z6.web.core.windows.net/verify?email=" + subscription.Email;
+        } else 
+        {
+            // window.location.href = "https://cateringstorage.z6.web.core.windows.net/verify?email=" + subscription.Email;
         }
     };
+}
+
+function getNotificationsTime() {
+    let notificationTimes = new Notificationtimes();
+    Object.keys(Days).forEach((day: string) => {
+        if ((<HTMLInputElement>document.getElementById('no_' + day)).checked === false) {
+            notificationTimes[day] = getNotificationTime(day);
+        }
+    });
+    return notificationTimes;
 }
 
 function verify() {
     let verification = new Verification();
     verification.Email = (<HTMLInputElement>document.getElementById('email')).value;
     verification.Token = (<HTMLInputElement>document.getElementById('token')).value;
-
 
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.open('POST', 'https://pxlcateringnotification.azurewebsites.net/api/verify', true);
@@ -80,7 +93,7 @@ function getEmailFromURL() {
     (<HTMLInputElement>document.getElementById('email')).value = email;
 }
 
-function checkInput() {
+function checkTokenInput() {
     const expression = /\S+@\S+/;
     if ((<HTMLInputElement>document.getElementById('token')).value.length === 6 && expression.test(String((<HTMLInputElement>document.getElementById('email')).value).toLowerCase())) {
         (<HTMLInputElement>document.getElementById('submit')).disabled = false;
@@ -89,11 +102,47 @@ function checkInput() {
     }
 }
 
-function checkInputEmail() {
+function checkEmailInput() {
     const expression = /\S+@\S+/;
     if (expression.test(String((<HTMLInputElement>document.getElementById('email')).value).toLowerCase())) {
         (<HTMLInputElement>document.getElementById('submit')).disabled = false;
     } else {
         (<HTMLInputElement>document.getElementById('submit')).disabled = true;
     }
+}
+
+function no_email_clicked(day: string) {
+    const timeElement = (<HTMLInputElement>document.getElementById(day));
+    const locationElement = (<HTMLSelectElement>document.getElementById(day + "_location"));
+    console.log(timeElement);
+    console.log(locationElement);
+    if ((<HTMLInputElement>document.getElementById('no_' + day)).checked === true) {
+        timeElement.value = "--:--";
+        timeElement.disabled = true;
+        locationElement.value = "";
+        locationElement.disabled = true;
+    } else {
+        timeElement.value = "11:45";
+        timeElement.disabled = false;
+        locationElement.selectedIndex = 0;
+        locationElement.disabled = false;
+    }
+}
+
+function getLocations() {
+    let locations = '{';
+
+    Object.keys(Days).forEach((day: string) => {
+        if ((<HTMLSelectElement>document.getElementById(day + "_location")).disabled === false) {
+            locations += '"' + day + '": "' + ((<HTMLSelectElement>document.getElementById(day + "_location")).selectedOptions[0].value) + '",';
+        }
+    });
+    locations = locations.substring(0, locations.length - 1) + '}'
+    console.log(locations);
+
+    if (locations.length < 5) {
+        return JSON.parse('{}');
+    }
+
+    return JSON.parse(locations);
 }
